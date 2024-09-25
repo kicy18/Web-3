1a19313270f9693bf8fd9b3bc238fa62216b013a
kicy <80158936+kicy18@users.noreply.github.com>
2024-09-25 16:52:26 +0530

    Setting up client folder

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+
+const App = ()=>{
+
+
+  return (
+    <div className="App">
+     <h1 className="text-5xl font-bold underline">
+    Hello world!
+  </h1>
+
+    </div>
+  )
+}
+
+export default App
